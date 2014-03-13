68d1e28f7621bbc9cc406125e2ec650c3a4865e2
Nils <nils.feyerabend@haw-hamburg.de>
2014-03-13 16:44:30 +0100

    Added NodeRegistry.cs to Project
    Start to plan NodeRegistry Component in WALK2.0.vpp

---
+++ 01 - Code/LIFE/Common/NodeRegistry/Implementation/NodeRegistry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NodeRegistry.Interface;
+
+namespace NodeRegistry.Implementation
+{
+    class NodeRegistry : INodeRegistry
+    {
+
+
+        public void startDiscovery()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void restartDiscovery()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CommonTypes.DataTypes.NodeInformationType> GetAllNodes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CommonTypes.DataTypes.NodeInformationType> GetAllNodesByType(CommonTypes.Types.NodeType nodeType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
