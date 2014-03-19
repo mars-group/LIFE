e56743bc0f6634400e72386b58ee229e8e8f2323
Christian Hüning <christian.huening@haw-hamburg.de>
2014-03-19 16:41:36 +0100

    XAP Test Added

---
+++ 03 - Prototyping/TupleSpaceTest/TupleSpaceStarter/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using GigaSpaces.Core.Metadata;
+
+namespace TupleSpaceStarter
+{
+    public class Person
+    {
+        [SpaceID]
+        public int? Ssn { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+    }
+}
---
+++ 03 - Prototyping/TupleSpaceTest/TupleSpaceStarter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using GigaSpaces.Core;
+using GigaSpaces.Core.Linq;
+
+namespace TupleSpaceStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var spaceProxy = GigaSpacesFactory.FindSpace("jini://*/*/myGrid?groups=$(XapNet.Groups)");
+
+
+            Console.WriteLine("Write (store) a couple of entries in the data grid:");
+            spaceProxy.Write(new Person { Ssn = 1, FirstName = "Vincent", LastName = "Chase" });
+            spaceProxy.Write(new Person { Ssn = 2, FirstName = "Johnny", LastName = "Drama" });
+
+            Console.WriteLine("Read (retrieve) an entry from the grid by its id:");
+            var result1 = spaceProxy.ReadById<Person>(1);
+
+            Console.WriteLine("Read an entry from the grid using LINQ:");
+            var query = from p in spaceProxy.Query<Person>()
+                        where p.FirstName == "Johnny"
+                        select p;
+            var result2 = spaceProxy.Read<Person>(query.ToSpaceQuery());
+
+            Console.WriteLine("Read all entries of type Person from the grid:");
+            Person[] results = spaceProxy.ReadMultiple(new Person());
+        }
+    }
+}
