a2d7543878a90c10eb0ade3a693dcbf5be22793b
Jason <wilmans@3ten.de>
2014-03-31 16:57:19 +0200

    An der Implementation von NodeRegistry

--- 01 - Code/LIFE/NodeRegistry/Implementation/NodeRegistryComponent.cs
+++ 01 - Code/LIFE/NodeRegistry/Implementation/NodeRegistryComponent.cs
@@ -1,8 +1,21 @@
-﻿using NodeRegistry.Interfaces;
+﻿using System.Collections.Generic;
+using CommonTypes.TransportTypes.SimulationControl;
+using NodeRegistry.Interfaces;
 
 namespace NodeRegistry.Implementation
 {
     public class NodeRegistryComponent : INodeRegistry
     {
+        private NodeManagementUseCase managementUseCase;
+
+        public NodeRegistryComponent()
+        {
+            managementUseCase = new NodeManagementUseCase();
+        }
+
+        public ICollection<NodeType> GetAllConnectedNodes()
+        {
+            return managementUseCase.GetAllConnectedNodes();
+        }
     }
 }
