5b211fcf7d36fa6a26c6570541ec05c02c1b0839
Jason <wilmans@3ten.de>
2014-04-08 15:02:27 +0200

    Test erweiter (kaum was gemacht)

--- 01 - Code/LIFE/SimulationManagerTest/TestModelContainer.cs
+++ 01 - Code/LIFE/SimulationManagerTest/TestModelContainer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,8 @@ namespace SimulationManagerTest
     {
         [Test]
         public void TestModelContentCopy() {
+            Directory.Delete("./testOrdner");
+
             ModelContent content = new ModelContent("./testOrdner");
 
             content.Write("./copiedOrdner");
