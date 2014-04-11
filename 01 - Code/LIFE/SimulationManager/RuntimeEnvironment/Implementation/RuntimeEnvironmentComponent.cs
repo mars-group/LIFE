1b9503c9afbfa22da3e09dafa2b73b5ffa66b2dd
Jason <wilmans@3ten.de>
2014-04-11 11:12:59 +0200

    Noch einen Fehler rausgehauen, wegen dem er nicht kompilieren wollte

--- 01 - Code/LIFE/SimulationManager/RuntimeEnvironment/Implementation/RuntimeEnvironmentComponent.cs
+++ 01 - Code/LIFE/SimulationManager/RuntimeEnvironment/Implementation/RuntimeEnvironmentComponent.cs
@@ -2,6 +2,7 @@
 using ConfigurationAdapter.Interface;
 using LayerRegistry.Interfaces;
 using ModelContainer.Interfaces;
+using NodeRegistry.Interface;
 using RuntimeEnvironment.Interfaces;
 using Shared;
 using SMConnector;
@@ -14,9 +15,29 @@ namespace RuntimeEnvironment.Implementation {
 
         public RuntimeEnvironmentComponent(Configuration<SimulationManagerSettings> settings,
                                             IModelContainer modelContainer,
-                                            ILayerRegistry layerRegistry)
+                                            INodeRegistry layerRegistry)
         {
             _runtimeEnvironmentUseCase = new RuntimeEnvironmentUseCase(settings, modelContainer, layerRegistry);
+        }
+
+        public void StartSimulationWithModel(TModelDescription model) {
+            ((IRuntimeEnvironment) _runtimeEnvironmentUseCase).StartSimulationWithModel(model);
+        }
+
+        public void PauseSimulation(TModelDescription model) {
+            ((IRuntimeEnvironment) _runtimeEnvironmentUseCase).PauseSimulation(model);
+        }
+
+        public void ResumeSimulation(TModelDescription model) {
+            ((IRuntimeEnvironment) _runtimeEnvironmentUseCase).ResumeSimulation(model);
+        }
+
+        public void AbortSimulation(TModelDescription model) {
+            ((IRuntimeEnvironment) _runtimeEnvironmentUseCase).AbortSimulation(model);
+        }
+
+        public void SubscribeForStatusUpdate(StatusUpdateAvailable statusUpdateAvailable) {
+            ((IRuntimeEnvironment) _runtimeEnvironmentUseCase).SubscribeForStatusUpdate(statusUpdateAvailable);
         }
     }
 }
