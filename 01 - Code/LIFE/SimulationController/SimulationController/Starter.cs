4b8073a396c0c0fa0e4047b7f30c30391e056136
Christian Hüning <christianhuening@googlemail.com>
2014-04-15 00:16:35 +0200

    kj

---
+++ 01 - Code/LIFE/SimulationController/SimulationController/Starter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulationController
+{
+    using SimulationController.Interface;
+
+    class Starter
+    {
+        public static void Main(string[] args) {
+            var useCase = new SimulationControllerUseCase();
+            var models = useCase.GetAllModels();
+            Console.WriteLine(models.First().Name);
+            Console.ReadLine();
+        }
+    }
+}
