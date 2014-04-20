83a19c90f0ff4fbc9c0b2beb469c4b4f16bab2ac
Christian Hüning <christianhuening@googlemail.com>
2014-04-20 12:55:54 +0200

    - ADDED: edge.js for mac / mono - UPDATED: Paper

--- 03 - Prototyping/EdgeJS Prototyp/test.js
+++ 03 - Prototyping/EdgeJS Prototyp/test.js
@@ -3,18 +3,8 @@ var edge = require('edge');
 
 
 var test = edge.func(function () {/*
-    using System.Threading.Tasks;
-    using System.Threading;
-
-    public class Startup
-    {
-        public async Task<object> Invoke(object input)
-        {
-            int v = (int)input;
-            var waitEvent = new ManualResetEventSlim(false);
-            waitEvent.Wait(2500);
-            return v + 2;
-        }
+    async (i) => {
+        return 42;
     }
 */});
 
