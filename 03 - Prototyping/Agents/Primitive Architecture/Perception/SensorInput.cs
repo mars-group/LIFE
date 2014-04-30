4de920cae3a0eff268da736155b99db2dedc2528
Jan Dalski <jan.dalski@web.de>
2014-04-30 16:43:23 +0200

    [Test] TeamExplorer VS2013 GIT integration.

--- 03 - Prototyping/Agents/Primitive Architecture/Perception/SensorInput.cs
+++ 03 - Prototyping/Agents/Primitive Architecture/Perception/SensorInput.cs
@@ -1,5 +1,8 @@
 ﻿namespace Primitive_Architecture.Perception {
   
+  /// <summary>
+  /// This class specifies the generic input for a sensory input type.
+  /// </summary>
   internal abstract class SensorInput : Input {
 
     public Sensor OriginSensor { get; private set; }
