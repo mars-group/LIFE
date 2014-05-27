8b6e7d07b55392b3095cd7b93f0ba0908569ad0a
Christian Hüning <christianhuening@googlemail.com>
2014-05-27 17:39:43 +0200

    - AbdoulayeV1 begonnen

---
+++ 01 - Code/LIFE/Examples/AbdoulayeTestV1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using NetTopologySuite.IO;
+using NetTopologySuite.Features;
+
+namespace AbdoulayeTestV1
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var json = new WebClient().DownloadString ("http://gis.3ten.de/GROUND/resrc/GROUND/data/togo/awrTreesComplete.json");
+			json = json.Replace("\r\n", string.Empty);
+			json = json.Replace (" ", String.Empty);
+			var pol = new GeoJsonReader ().Read<FeatureCollection> (json);
+
+			Console.ReadLine ();
+		}
+	}
+}
