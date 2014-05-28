4f4bbd482e4ec0f292f87b1e29bc58960ce2c74e
Christian Hüning <christian.huening@haw-hamburg.de>
2014-05-28 17:42:09 +0200

    - stuff

--- 01 - Code/LIFE/Examples/AbdoulayeTestV1/Program.cs
+++ 01 - Code/LIFE/Examples/AbdoulayeTestV1/Program.cs
@@ -12,18 +12,16 @@ namespace AbdoulayeTestV1
 		public static void Main (string[] args)
 		{
 			//var json = new WebClient().DownloadString ("http://gis.3ten.de/GROUND/resrc/GROUND/data/togo/awrTreesComplete.json");
-		    var txt = new StringBuilder();
-            using (FileStream fs = File.Open("./awrTreesComplete.json", FileMode.Open))
-            {
-                byte[] b = new byte[1024];
-                UTF8Encoding temp = new UTF8Encoding(true);
-                
 
-                while (fs.Read(b, 0, b.Length) > 0) {
-                    txt.Append(temp.GetString(b));
-                }
+            
+
+		    var json = "";
+            using (TextReader txtReader = new StreamReader(@"./awrMini.json"))
+            {
+                json = txtReader.ReadToEnd();
             }
-		    var json = txt.ToString();
+		    //var i = 1;
+            //json = "{\"features\":[{\"type\":\"Feature\",\"geometry\":{\"type\":\"Point\",\"coordinates\":[23.0,56.0]},\"properties\":{\"test1\":\"value1\"}}],\"type\":\"FeatureCollection\",\"crs\":{\"type\":\"name\",\"properties\":{\"name\":\"name1\"}}}";
 			json = json.Replace("\r\n", string.Empty);
 			json = json.Replace (" ", String.Empty);
 			var pol = new GeoJsonReader ().Read<FeatureCollection> (json);
