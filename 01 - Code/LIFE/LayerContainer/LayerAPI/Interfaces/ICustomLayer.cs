abf099413d40cbcbb459a339d800d1c3bd1e0f28
Christian Hüning <christianhuening@googlemail.com>
2014-05-31 10:10:42 +0200

    - Sorry to say, but 4.5.1 does not work with Xamarin Studio and Mono --> Changed back to .NET 4.5 Profile for SimulationManager

--- 01 - Code/LIFE/LayerContainer/LayerAPI/Interfaces/ICustomLayer.cs
+++
@@ -1,9 +0,0 @@
-﻿using Mono.Addins;
-
-namespace LayerAPI.Interfaces {
-    /// <summary>
-    ///     A marker interface. Extend this when building your own Layer-Interface.
-    /// </summary>
-    [TypeExtensionPoint]
-    public interface ICustomLayer {}
-}
