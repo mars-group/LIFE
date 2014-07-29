e35218314edbb541721ba2c46bd70e358cbcbb3f
Daniela Niemeyer <daniela.niemeyer@haw-hamburg.de>
2014-07-29 16:49:08 +0200

    Prototyping goap planner

---
+++ 03 - Prototyping/MARS.LIFE.GOAP/GoapActionSystemTest/GoapActionSystemTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using GoapActionSystem.Implementation;
+using GoapCommon.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GoapActionSystemTest
+{
+    [TestClass]
+    public class GoapActionSystemTest
+    {
+        [TestMethod]
+        public void TestMethod1() {
+            Assert.IsFalse(false);
+        }
+    }
+
+    [TestClass]
+    public class PlannerTest {
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestPlannerConstructorWithEmptyActionList() {
+            new GoapPlanner(1, new List<IGoapAction>());
+        }
+    }
+}
