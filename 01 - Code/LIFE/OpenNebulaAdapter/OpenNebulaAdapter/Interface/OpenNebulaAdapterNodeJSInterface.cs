37ba7af64de6f0bd11d27842011d7436b160c580
Christian Hüning <christian.huening@haw-hamburg.de>
2014-08-21 16:39:28 +0200

    openNebula adapter works

--- 01 - Code/LIFE/OpenNebulaAdapter/OpenNebulaAdapter/Interface/OpenNebulaAdapterNodeJSInterface.cs
+++ 01 - Code/LIFE/OpenNebulaAdapter/OpenNebulaAdapter/Interface/OpenNebulaAdapterNodeJSInterface.cs
@@ -6,20 +6,24 @@ namespace OpenNebulaAdapter.Interface
     public class OpenNebulaAdapterNodeJSInterface
     {
 
-        public static void main(String[] args) {
+        public static void Main(String[] args) {
             // First create the client
-            string proxyUrl = "<YOUR_SERVER_URL>";
-            string adminUser = "<YOUR_ADMIN_USERNAME>"; //should be user with driver server_* to allow requests delegation
-            string adminPwd = "<YOUR_ADMIN_PASSWORD>"; //SHA1 password
+            const string proxyUrl = "http://141.22.29.2:2633/RPC2";
+            const string adminUser = "serveradmin"; //should be user with driver server_* to allow requests delegation
+            const string adminPwd = "80051ee6a7b403ae88cb1fa8e5d9d0877eddfbc0"; //SHA1 password
+            
             var one = new OneClient(proxyUrl, adminUser, adminPwd);
             
             // Do a request as admin
             USER_POOL pool = one.UserGetPoolInfo();
+            var templateInfo = one.TemplateGetInfo(15);
 
+            
             // Do a request on behalf of a normal user
-            string targetUser = "<YOUR_TARGET_USERNAME>";
+            string targetUser = "christian";
             one.StartDelegate(targetUser);
-            //int RemoteId = one.TemplateInstanciateVM(idTemplate, vmName, false, "");
+            
+            int RemoteId = one.TemplateInstanciateVM(15, "TestVM", false, "");
             one.EndDelegate();
         }
     }
