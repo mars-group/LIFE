f156a2d4e2d210f5f3bfe298ea603da185c9f389
Daniela Niemeyer <daniela.niemeyer@haw-hamburg.de>
2014-09-25 10:15:48 +0200

    [goap] corrected test actions

--- 01 - Code/LIFE/LIFE Services/Agents/GoalOrientedActionPlanning/GoapModelTest/Actions/ActionClean.cs
+++ 01 - Code/LIFE/LIFE Services/Agents/GoalOrientedActionPlanning/GoapModelTest/Actions/ActionClean.cs
@@ -5,17 +5,13 @@ using GoapCommon.Interfaces;
 using GoapModelTest.Worldstates;
 
 namespace GoapModelTest.Actions {
-
     public class ActionClean : AbstractGoapAction {
-
-        public ActionClean(List<IGoapWorldstate> preconditionWorldstates, List<IGoapWorldstate> effectWorldstates)
-            : base(preconditionWorldstates, effectWorldstates) {}
-
         public ActionClean()
-            : base(new List<IGoapWorldstate> {new Happy(true)}, new List<IGoapWorldstate> {
-                new Happy(false),
-                new HasMoney(true)
-            }) {}
+            : base(new List<IGoapWorldstate> {new Happy(true)},
+                new List<IGoapWorldstate> {
+                    new Happy(false),
+                    new HasMoney(true)
+                }) {}
 
         public override bool ValidateContextPreconditions() {
             return true;
