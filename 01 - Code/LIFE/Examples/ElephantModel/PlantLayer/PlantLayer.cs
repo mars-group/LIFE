6d81c12d2e435ce074a37a72053d259989f4ad46
Matthew Pearson <mattpearson13@gmail.com>
2014-09-30 11:53:33 -0400

    Elephant Changes

--- 01 - Code/LIFE/Examples/ElephantModel/PlantLayer/PlantLayer.cs
+++ 01 - Code/LIFE/Examples/ElephantModel/PlantLayer/PlantLayer.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ElephantLayer
+namespace PlantLayer
 {
-    public class Class1
+    class PlantLayer
     {
     }
 }
--- 01 - Code/LIFE/Examples/ElephantModel/WaterLayer/WaterLayer.cs
+++ 01 - Code/LIFE/Examples/ElephantModel/WaterLayer/WaterLayer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WaterLayer
 {
-    public class Class1
+    class WaterLayer
     {
     }
 }
