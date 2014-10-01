3d4797aa427a6e3985d43f58e33be378bc42048b
Christian Hüning <christian.huening@haw-hamburg.de>
2014-10-01 16:15:06 -0400

    - ADDED: Waterhole implementation

---
+++ 01 - Code/LIFE/Examples/ElephantModel/WaterLayer/Agents/Waterhole.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using CSharpQuadTree;
+using LayerAPI.Interfaces;
+
+namespace WaterLayer
+{
+	public class Waterhole: IAgent, IQuadObject
+	{
+		private double _capacity;
+
+		public Waterhole ()
+		{
+			_capacity = 100.0;
+		}
+
+		public double Capacity { get { return _capacity; } }
+
+		public double TakeWater(double amount){
+			var res = _capacity - amount;
+			if (res >= 0) {
+				_capacity = res;
+				return amount;
+			} else {
+				_capacity = 0;
+				return amount - res;
+			}
+		}
+
+		#region ITickClient implementation
+
+		public void Tick ()
+		{
+			// just reset for now, infinite waterhole
+			_capacity = 100;
+		}
+
+		#endregion
+	}
+}
+
--- 01 - Code/LIFE/Examples/ElephantModel/WaterLayer/WaterLayer.cs
+++ 01 - Code/LIFE/Examples/ElephantModel/WaterLayer/WaterLayer.cs
@@ -5,16 +5,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TwoDimEnvironment;
+using System.Drawing;
 
 [assembly: Addin]
 [assembly: AddinDependency("LayerContainer", "0.1")]
 
 namespace WaterLayer
 {
-    class WaterLayer : ISteppedLayer
+    public class WaterLayer : ISteppedLayer
     {
+		private ITwoDimEnvironment<Waterhole> environment;
+
+		private List<Waterhole> _waterholes;
+
         public bool InitLayer<I>(I layerInitData, RegisterAgent registerAgentHandle, UnregisterAgent unregisterAgentHandle) {
-            throw new NotImplementedException();
+			environment = new TwoDimEnvironmentUseCase<Waterhole>();
+			_waterholes = new List<Waterhole>();
+
+			var p = new Waterhole(48, 52, new Size (5.0, 5.0));
+			registerAgentHandle.Invoke (this, p);
+			_waterholes.Add (p);
+			environment.Add(p);
+
+			return true;
         }
 
         public long GetCurrentTick() {
