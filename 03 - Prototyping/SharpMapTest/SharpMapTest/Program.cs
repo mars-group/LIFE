935ae92afa3f0ec371e09671c91ac8690be40717
Christian Hüning <christian.huening@haw-hamburg.de>
2014-11-11 09:59:35 +0100

    asd

--- 03 - Prototyping/SharpMapTest/SharpMapTest/Program.cs
+++ 03 - Prototyping/SharpMapTest/SharpMapTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using GeoAPI.Geometries;
@@ -17,27 +18,47 @@ namespace SharpMapTest
 
             var myMap = new Map {
                 Size = rasterLayer.Size,
-                MinimumZoom = 5,
-                BackColor = Color.White,
+                MinimumZoom = 1,
+                BackColor = Color.Black,
             };
 
             myMap.Layers.Add(rasterLayer);
             myMap.ZoomToExtents();
             var projectedCoordinate = myMap.ImageToWorld(new PointF(2000, 5000));
+            var polygon =
+                new Polygon(
+                    new LinearRing(new Coordinate[]
+                        {
+                         myMap.ImageToWorld(new PointF(2000, 5000)), 
+                         myMap.ImageToWorld(new PointF(4000, 5000)),
+                         myMap.ImageToWorld(new PointF(2000, 7500)),
+                         myMap.ImageToWorld(new PointF(4000, 7500)),
+                         myMap.ImageToWorld(new PointF(2000, 5000))
+                        }
+                    )
+                );
 
-
-            var featureSet = new FeatureDataSet();
             
+            var featureSet = new FeatureDataSet();
+
+            var sw = Stopwatch.StartNew();
+            sw.Start();
             rasterLayer.ExecuteIntersectionQuery(new NetTopologySuite.Geometries.Point(31, -23), featureSet);
-
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
             var featureSet2 = new FeatureDataSet();
+            sw = Stopwatch.StartNew();
+            sw.Start();
 
-            rasterLayer.ExecuteIntersectionQuery(new NetTopologySuite.Geometries.Point(projectedCoordinate.X, projectedCoordinate.Y) , featureSet2);
-
-            myMap.ZoomToExtents();
-            Image imgMap = myMap.GetMap();
+            rasterLayer.ExecuteIntersectionQuery(polygon, featureSet2);
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+            //myMap.ZoomToExtents();
+            /**Image imgMap = myMap.GetMap();
             imgMap.Save(@".\mymap.png", ImageFormat.Png);
             Process.Start(@"E:\SoftwareProjekte\LIFE\03 - Prototyping\SharpMapTest\SharpMapTest\bin\Debug\mymap.png");
+             */
+            Console.ReadLine();
         }
     }
 }
