8c719f4e3db83042522e0acebdb9504977328fde
Christian Hüning <christianhuening@googlemail.com>
2014-11-11 07:56:50 +0100

    - transformation added

--- 03 - Prototyping/SharpMapTest/SharpMapTest/Program.cs
+++ 03 - Prototyping/SharpMapTest/SharpMapTest/Program.cs
@@ -13,8 +13,8 @@ namespace SharpMapTest
     {
         static void Main(string[] args)
         {
-            var rasterLayer = new GdalRasterLayer("elevationLayer", @".\GISData\knp_srtm90m.asc");
-            new VectorLayer("MyLayer");
+            var rasterLayer = new GdalRasterLayer("elevationLayer", @"..\..\..\GISData\knp_srtm90m.asc");
+
             var myMap = new Map {
                 Size = rasterLayer.Size,
                 MinimumZoom = 5,
@@ -22,11 +22,18 @@ namespace SharpMapTest
             };
 
             myMap.Layers.Add(rasterLayer);
-            
+            myMap.ZoomToExtents();
+            var projectedCoordinate = myMap.ImageToWorld(new PointF(2000, 5000));
+
+
             var featureSet = new FeatureDataSet();
             
             rasterLayer.ExecuteIntersectionQuery(new NetTopologySuite.Geometries.Point(31, -23), featureSet);
-            
+
+            var featureSet2 = new FeatureDataSet();
+
+            rasterLayer.ExecuteIntersectionQuery(new NetTopologySuite.Geometries.Point(projectedCoordinate.X, projectedCoordinate.Y) , featureSet2);
+
             myMap.ZoomToExtents();
             Image imgMap = myMap.GetMap();
             imgMap.Save(@".\mymap.png", ImageFormat.Png);
