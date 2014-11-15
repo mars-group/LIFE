a5981820292a3f77ef2fef37c1dd25d2e20d8da6
Christian Hüning <christian.huening@haw-hamburg.de>
2014-11-15 11:07:49 +0100

    - Fixed Type parameter error in IGISPAssiveLAyer

--- 01 - Code/LIFE/LIFE API/LayerAPI/Interfaces/IGISPassiveLayer.cs
+++ 01 - Code/LIFE/LIFE API/LayerAPI/Interfaces/IGISPassiveLayer.cs
@@ -6,7 +6,6 @@ namespace LayerAPI.Interfaces
     /// A passive GIS layer, which allows to load and access GIS data
     /// as well as hold agents.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public interface IGISPassiveLayer<T> : ISteppedLayer, IGISAccess<T> {
+    public interface IGISPassiveLayer : ISteppedLayer, IGISAccess {
     }
 }
