9b8ca211def1636635930e65ab246dfa5900b6ef
Christian Hüning <christianhuening@googlemail.com>
2015-01-20 10:51:35 +0100

    Added OneTickTimeSpan to ModelConfig and TInitData

--- 01 - Code/LIFE/LIFE API/LifeAPI/Config/ModelConfig.cs
+++ 01 - Code/LIFE/LIFE API/LifeAPI/Config/ModelConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 
 namespace LifeAPI.Config
 {
@@ -8,12 +9,33 @@ namespace LifeAPI.Config
     {
         public List<LayerConfig> LayerConfigs { get; set; }
 
+        private TimeSpan _oneTickTimeSpan { get; set; }
+
+        // Public Property - XmlIgnore as it doesn't serialize anyway
+        [XmlIgnore]
+        public TimeSpan OneTickTimeSpan
+        {
+            get { return _oneTickTimeSpan; }
+            set { _oneTickTimeSpan = value; }
+        }
+
+        // Pretend property for serialization
+        [XmlElement("OneTickTimeSpan")]
+        public string OneTickTimeSpanTicks
+        {
+            get { return _oneTickTimeSpan.ToString(); }
+            set { _oneTickTimeSpan = TimeSpan.Parse(value); }
+        }
+
+
         public ModelConfig(List<LayerConfig> layerConfigs)
         {
+            _oneTickTimeSpan = new TimeSpan(0, 0, 0, 1, 0);
             LayerConfigs = layerConfigs;
         }
 
         public ModelConfig() {
+            _oneTickTimeSpan = new TimeSpan(0, 0, 0, 1, 0);
             LayerConfigs = new List<LayerConfig>();
         }
 
