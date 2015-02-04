b88f354c3ba52e5dd9b799c406072fd5df0d10bc
Jan Dalski <jan.dalski@web.de>
2015-02-04 20:21:04 +0100

    Comparison tests added.

--- 01 - Code/LIFE/Tests/DalskiAgentTests/Env25Test.cs
+++ 01 - Code/LIFE/Tests/DalskiAgentTests/Env25Test.cs
@@ -1,8 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using DalskiAgent.Auxiliary.Environment;
 using NUnit.Framework;
+using SpatialAPI.Entities.Transformation;
 
-namespace AgentTests {
+namespace DalskiAgentTests {
 
   [TestFixture]
   public class Env25Test {
@@ -17,6 +20,28 @@ namespace AgentTests {
       // Create environment and desired number of spatial entities (unpositioned, 1x1 dimension).
       Env25 env = new Env25(envWidth, envHeight);
       Parallel.For(0, nrAgents, i1 => env.AddWithRandomPosition(new Obj(new Float2(0, 0), new Float2(1, 1))));
+    }
+
+
+    [Test]  // Do a number of insertion tests to measure general performance.
+    public void PerformanceTest() {
+          
+      // Test series.
+      int[] tests = {1000, 2000, 4000, 8000, 16000};
+      const int envWidth  = 1000;  // Important: Ensure that width*height
+      const int envHeight = 1000;  // is sufficient to store max. number of agents!
+
+      Stopwatch stopwatch = Stopwatch.StartNew();
+
+      // Perform tests and output elapsed time.
+      for (int t = 0; t < tests.Length; t++) {
+        Env25 env = new Env25(envWidth, envHeight);
+        for (int i = 0; i < tests[t]; i++) {
+          Assert.True(env.Add(new Obj(new Float2(0, 0), new Float2(0.9f, 0.9f)), new Vector3(i%envWidth, (float)i/envWidth)));
+        } 
+        Console.WriteLine("["+tests[t]+" agents]: "+stopwatch.ElapsedMilliseconds + " ms");
+        stopwatch.Restart();  // Reset stopwatch.
+      }        
     }
 
 
