425a1630ec224eaa6a4933fd793a71b8ab3614fb
Daniela Niemeyer <daniela.niemeyer@haw-hamburg.de>
2015-02-10 10:20:32 +0100

    MARS Workshop - initial Marula setup

---
+++ 01 - Code/LIFE/Examples/ArsAfrika/Savanne/Agents/Marula.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using LifeAPI.Agent;
+using SpatialAPI.Entities;
+using SpatialAPI.Entities.Movement;
+using SpatialAPI.Environment;
+
+namespace Savanne.Agents {
+    internal class Marula : IAgent {
+        public ISpatialEntity SpacialTreeEntity;
+        private IEnvironment _esc;
+
+        public Marula(double x, double y, IEnvironment esc) {
+            ID = Guid.NewGuid();
+            SpacialTreeEntity = new SpatialTreeEntity(x, y, CollisionType.SelfCollision);
+            _esc = esc;
+        }
+
+        #region IAgent Members
+
+        public Guid ID { get; set; }
+
+
+        public void Tick() {
+            Console.WriteLine("Hello Tree");
+        }
+
+        #endregion
+    }
+}
---
+++ 01 - Code/LIFE/Examples/ArsAfrika/Savanne/SavanneLayerImpl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using LCConnector.TransportTypes;
+using LifeAPI.Agent;
+using LifeAPI.Layer;
+using Mono.Addins;
+using Savanne.Agents;
+using SpatialAPI.Entities;
+using SpatialAPI.Entities.Transformation;
+using SpatialAPI.Environment;
+
+[assembly: Addin]
+[assembly: AddinDependency("LayerContainer", "0.1")]
+
+namespace Savanne {
+    [Extension(typeof (ISteppedLayer))]
+    internal class SavanneLayerImpl : ISteppedActiveLayer {
+        private readonly List<IAgent> _allAgentsOnLayer = new List<IAgent>();
+        private long _tick;
+        public IEnvironment Esc = new EnvironmentServiceComponent.Implementation.EnvironmentServiceComponent();
+
+        #region ISteppedActiveLayer Members
+
+        public bool InitLayer(TInitData layerInitData, RegisterAgent registerAgentHandle,
+            UnregisterAgent unregisterAgentHandle) {
+            Console.WriteLine("Starting 1 agent ...");
+
+            for (int i = 1; i < 10; i++) {
+                Marula ourAwesomeMarula = new Marula(i*10, i*10, Esc);
+                _allAgentsOnLayer.Add(ourAwesomeMarula);
+                registerAgentHandle.Invoke(this, ourAwesomeMarula);
+                Esc.Add(ourAwesomeMarula.SpacialTreeEntity, Vector3.Zero);
+                IEnumerable<ISpatialEntity> test = Esc.ExploreAll();
+            }
+            
+            Console.WriteLine("Finished agent...");
+            return true;
+        }
+
+        public long GetCurrentTick() {
+            return _tick;
+        }
+
+        public void SetCurrentTick(long currentTick) {
+            _tick = currentTick;
+        }
+
+        public void Tick() {}
+
+        public void PreTick() {}
+
+        public void PostTick() {}
+
+        #endregion
+    }
+}
