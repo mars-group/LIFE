d997f6053c8801df4a7d65ce2547fbd099c3aadc
Christian Hüning <christianhuening@googlemail.com>
2015-06-26 08:16:57 +0200

    - added ITimeSeries interface

---
+++ 01 - Code/LIFE/LIFE API/LifeAPI/Layer/TimeSeries/ITimeSeriesLayer.cs
@@ -0,0 +1,12 @@
+﻿namespace LifeAPI.Layer.TimeSeries {
+
+    public interface ITimeSeriesLayer : ISteppedLayer {
+        /// <summary>
+        /// Get a value for the current simulation time.
+        /// </summary>
+        /// <returns>The value for the current simulation time. Returns null, if the stored value is empty or explicit null. 
+        /// Returns a NoDataFound object, if the there is no value for the requested simulation time</returns>
+        object GetValueForCurrentSimulationTime();
+    }
+
+}
