22d03a112c64b4d9d78237553531a85b5a035a4d
Christian Hüning <christian.huening@haw-hamburg.de>
2016-08-22 18:40:57 +0200

    more work

--- 01-Code/LIFEv2/Common/CommonTypes/DataTypes/TNodeInformation.cs
+++ 01-Code/LIFEv2/Common/CommonTypes/DataTypes/TNodeInformation.cs
@@ -9,19 +9,17 @@
 using System;
 using System.Text;
 using CommonTypes.Types;
-using ProtoBuf;
 
 namespace CommonTypes.DataTypes {
-    [ProtoContract]
-    [Serializable]
+
     public class TNodeInformation : IComparable {
-        [ProtoMember(1)]
+
         public NodeType NodeType { get; private set; }
 
-        [ProtoMember(2)]
+
         public string NodeIdentifier { get; private set; }
 
-        [ProtoMember(3)]
+
         public NodeEndpoint NodeEndpoint { get; private set; }
 
 
