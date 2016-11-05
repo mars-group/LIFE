eedc29ec4b07582ad4f22e2e8322945cd432b82f
Christian Hüning <christian.huening@haw-hamburg.de>
2016-11-05 12:33:05 +0100

    updated to new geocommons

--- 01-Code/LIFEv2/Common/LayerLoader/Implementation/LayerLoader.cs
+++ 01-Code/LIFEv2/Common/LayerLoader/Implementation/LayerLoader.cs
@@ -6,7 +6,6 @@ using System.Reflection;
 using LayerLoader.Interface;
 using LayerLoader.Interface.Exceptions;
 using LCConnector.TransportTypes.ModelStructure;
-using LifeAPI.Layer;
 
 namespace LayerLoader.Implementation
 {
@@ -21,15 +20,34 @@ namespace LayerLoader.Implementation
 
         private LIFEAssemblyLoader _asl;
 
+        private readonly List<LayerTypeInfo> _simulationManagerLayerCache;
+
+        public LayerLoader(string modelPath = "./model")
+        {
+            _simulationManagerLayerCache  = new List<LayerTypeInfo>();
+            _asl = new LIFEAssemblyLoader(modelPath);
+        }
+
+        /// <summary>
+        /// Gets called on LayerContainer only!
+        /// </summary>
+        /// <param name="modelContent"></param>
         public void LoadModelContent(ModelContent modelContent)
         {
             //write files
             modelContent.Write(_pathForTransferredModel);
             _asl  = new LIFEAssemblyLoader(_pathForTransferredModel);
-            _layerTypes
-                .AddRange(DoReflection(_pathForTransferredModel));
+            _layerTypes.AddRange(DoReflection(_pathForTransferredModel));
         }
 
+        /// <summary>
+        /// Gets called on LayerContainer only!
+        /// </summary>
+        /// <param name="layerName"></param>
+        /// <returns></returns>
+        /// <exception cref="ModelCodeFailedToLoadException"></exception>
+        /// <exception cref="LayerNotFoundException"></exception>
+        /// <exception cref="ConstructorNotFoundException"></exception>
         public LayerTypeInfo LoadLayerOnLayerContainer(string layerName)
         {
             if (_layerTypes.Count <= 0)
@@ -53,58 +71,71 @@ namespace LayerLoader.Implementation
         }
 
 
-
+        /// <summary>
+        /// Gets called from SimulationManager only!
+        /// </summary>
+        /// <param name="modelPath"></param>
+        /// <returns></returns>
+        /// <exception cref="ModelCodeFailedToLoadException"></exception>
         public IEnumerable<LayerTypeInfo> LoadAllLayersForModel(string modelPath)
         {
+            // cache results, no need to do this more than once
+            if (_simulationManagerLayerCache.Any())
+            {
+                return _simulationManagerLayerCache;
+            }
 
-            var results = new List<LayerTypeInfo>();
-
-            _asl = new LIFEAssemblyLoader(modelPath);
+            //_asl = new LIFEAssemblyLoader(modelPath);
 
             var foundLayerTypes = DoReflection(modelPath)
                 .Select(layerType => new LayerTypeInfo(layerType, layerType.GetConstructors()))
                 .ToList();
-            results.AddRange(foundLayerTypes);
+            _simulationManagerLayerCache.AddRange(foundLayerTypes);
 
 
-            if (!results.Any())
+            if (!_simulationManagerLayerCache.Any())
             {
                 throw new ModelCodeFailedToLoadException($"It appears there was no valid mode code found in the {modelPath} subdirectory. Please check your build config etc.!");
             }
-            return results;
+            return _simulationManagerLayerCache;
         }
 
         private IEnumerable<Type> DoReflection(string modelPath)
         {
             var types = new List<Type>();
             // iterate all DLLs and try to find ILayer implementations
-            foreach (var fileSystemInfo in new DirectoryInfo(modelPath).GetFileSystemInfos("*.dll"))
+            foreach (var fileSystemInfo in new DirectoryInfo(modelPath).GetFileSystemInfos("*.dll", SearchOption.AllDirectories))
             {
 
                 try
                 {
                     var asm = _asl.LoadFromAssemblyPath(fileSystemInfo.FullName);
                     types.AddRange(asm.GetTypes()
-                        .Where(t =>
-                                    t.GetTypeInfo().IsClass
-                                    &&
-                                    t.GetInterfaces().Contains(typeof(ILayer))
-                                    &&
-                                    !t.GetTypeInfo().IsAbstract
+                        .Where(t => t.GetTypeInfo().GetInterface("ILayer") != null && !t.GetTypeInfo().IsAbstract
+                            /*
+                                t.GetTypeInfo().IsClass
+                                &&
+                                t.GetInterfaces().Contains(typeof(ILayer))
+                                &&
+                                !t.GetTypeInfo().IsAbstract
+*/
                         ).ToList());
 
                 }
                 catch (ReflectionTypeLoadException ex)
                 {
-                    Console.WriteLine($"Caught type load error while Loading model code. Make sure you use 'dotnet publish' to finally create your model code. Error was: {ex.LoaderExceptions.First()}");
+                    Console.WriteLine($"Caught type load error while Loading model code. Error was: {ex.LoaderExceptions.First()}");
+                    //throw ex;
                 }
                 catch (FileLoadException fex)
                 {
                     Console.WriteLine($"Caught a FileLoadException. Msg was: {fex.Message}, Error was: {fex.InnerException}");
+                    //throw fex;
                 }
                 catch (BadImageFormatException bex)
                 {
                     Console.WriteLine($"Caught a BadImageFormatException. File was: {bex.FileName}, Msg was: {bex.Message}");
+                    //throw bex;
                 }
 
             }
--- 01-Code/LIFEv2/LIFE API/LifeAPI/Layer/Obstacle/IObstacleLayer.cs
+++ 01-Code/LIFEv2/LIFE API/LifeAPI/Layer/Obstacle/IObstacleLayer.cs
@@ -1,4 +1,4 @@
-﻿using GeoGridEnvironment.Interface;
+﻿using GeoCommon;
 
 namespace LifeAPI.Layer.Obstacle
 {
