3d54a18ef27c226d4901866dcaa7428898e3da89
Christian Hüning <christian.huening@haw-hamburg.de>
2017-03-01 09:06:39 +0100

    readded all agent classes from refactoring

---
+++ LIFE-Components/Agents/DalskiAgent/Reasoning/MovementAction.cs
@@ -0,0 +1,33 @@
+﻿namespace LIFE.Components.Agents.DalskiAgent.Reasoning {
+  
+  /// <summary>
+  ///   Action object for agent movement.
+  /// </summary>
+  public class MovementAction : IInteraction {
+
+    private readonly MovementFunction _movementFkt;  // Movement function to execute.
+
+    /// <summary>
+    ///   Create a new movement action.
+    /// </summary>
+    /// <param name="movementFkt">Movement function to execute.</param>
+    public MovementAction(MovementFunction movementFkt) {
+      _movementFkt = movementFkt;
+    }
+
+
+    /// <summary>
+    ///   Executes the movement.
+    /// </summary>
+    public void Execute() {
+      _movementFkt();
+    }
+
+
+    /// <summary>
+    ///   This function holds the instructions how to perform this movement.
+    ///   It is set in the concrete mover modules.
+    /// </summary>
+    public delegate void MovementFunction();
+  }
+}
