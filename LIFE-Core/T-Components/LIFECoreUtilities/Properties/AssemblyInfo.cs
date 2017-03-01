1d1ca3afb3b4d646caf904102beb99640e683ed7
Christian Hüning <christian.huening@haw-hamburg.de>
2017-03-01 07:56:58 +0100

    divided utilities into two packages and made an exclusive one for LIFE-Core so that LIFE-Core does not have a dependency on LIFE-Components (also Hashin, Random and McastGen are not used elsewhere)

---
+++ LIFE-Core/T-Components/LIFECoreUtilities/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("LIFECoreUtilities")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LIFECoreUtilities")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("7D098664-E07F-4D94-91A2-9A3E3B91AB75")]
