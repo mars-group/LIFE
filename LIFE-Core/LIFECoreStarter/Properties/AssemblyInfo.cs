25b861683d3f78f9f110981b9eb2c8530e0aeb33
Christian Hüning <christian.huening@haw-hamburg.de>
2017-03-01 07:44:28 +0100

    added output of all libs to project.json and renamed MARSLocalStarter to LIFECoreStarter also renamed resulting nuget package to "LIFE-Core"

--- LIFE-Core/LIFECoreStarter/Properties/AssemblyInfo.cs
+++ LIFE-Core/LIFECoreStarter/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("MARSLocalStarter")]
+[assembly: AssemblyTitle("LIFECoreStarter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
